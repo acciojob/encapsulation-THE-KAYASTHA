58e5c3be51bee166f24c0647765628e7b86053a3
kamal <tekkensingle@gmail.com>
2023-12-14 18:57:56 +0530

    excaps

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args)
+    {
+        RWOnly obj=new RWOnly();
+
+        obj.setName("kamal");
+
+        //java: <identifier> expected
+
+
+        String ans= obj.getName();
+
+
+    }
+
+
+
 }
